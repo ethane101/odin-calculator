45ef8b6672b51b2fc4b4b78d11c995fc519c8e79
Ethan David Escario <escarioethandavidh@gmail.com>
2025-08-01 01:34:04 -0400

    Constructed the eventListeners for all buttons. Functionality is applied to the AC and number buttons, but the operator buttons are not yet functional. Next step is to implement operator and evaluation logic.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// add the buttons to the calculator in JS to make attaching event listeners easier
+// add the buttons to the calculator in JS to make attaching event 
+// listeners easier
 const topSection = document.querySelector("#top-sect");
 const clearButton = document.createElement("button");
 clearButton.id = "AC";
@@ -9,6 +10,7 @@ topSection.appendChild(clearButton)
 const buttonBgColors = ["#FFCBE1", "#D6E5BD", "#F9E1A8", "#DCCCEC", "#FFDAB4"];
 const buttonBorderColors = ["#876d78ff", "#879275ff", "#938463ff", "#8b7d98ff", "#9f8266ff"];
 
+// add number buttons
 const numpadNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const numbers = document.querySelector("#numbers");
 
@@ -21,6 +23,7 @@ numpadNumbers.forEach(num => {
     numbers.appendChild(button);
 });
 
+// add operator buttons
 const operatorSymbols = ['+', '-', 'x', '/', '='];
 const operators = document.querySelector("#operators");
 
@@ -33,4 +36,26 @@ operatorSymbols.forEach(operator => {
     button.style = `background-color: ${buttonBgColors[operatorCount]}; border: 0.25vw solid ${buttonBorderColors[operatorCount]};`;
     operators.appendChild(button);
     operatorCount++;
+});
+
+// create mechanism for keeping track of the number and operation
+// maybe have a virtual element representing the screen element?
+
+const screen = document.querySelector("#screen");
+let screenContent = "";
+
+numbers.addEventListener('click', (e) => {
+    let num = e.target.id;
+    screenContent += num;
+    screen.textContent = screenContent;
+});
+
+clearButton.addEventListener('click', (e) => {
+    screenContent = "";
+    screen.textContent = screenContent;
+});
+
+operators.addEventListener('click', (e) => {
+    let operator = e.target.id;
+    console.log(operator);
 });
