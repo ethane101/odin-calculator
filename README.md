db983b155915756e8c0f157f2f3c377e0b154633
Ethan David Escario <escarioethandavidh@gmail.com>
2025-08-01 01:17:05 -0400

    Completed formatting and styling the buttons and calculator UI. The design is scalable based on screen width. Buttons were made in JavaScript instead of HTML to make it easier to add event listeners and responsive design later on.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # odin-calculator
+Buttons to Include:
+- Add (+), Subtract(-), Multiply(x), Divide(/) DONE
+- 0 - 9 DONE
+- Equals (=) DONE   
+- Clear (AC)
+
+*Extra Credit: "Delete" button and "." button for decimals
---
+++ script.js
@@ -0,0 +1,36 @@
+// add the buttons to the calculator in JS to make attaching event listeners easier
+const topSection = document.querySelector("#top-sect");
+const clearButton = document.createElement("button");
+clearButton.id = "AC";
+clearButton.textContent = "AC";
+topSection.appendChild(clearButton)
+
+// different color for each button row
+const buttonBgColors = ["#FFCBE1", "#D6E5BD", "#F9E1A8", "#DCCCEC", "#FFDAB4"];
+const buttonBorderColors = ["#876d78ff", "#879275ff", "#938463ff", "#8b7d98ff", "#9f8266ff"];
+
+const numpadNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = document.querySelector("#numbers");
+
+numpadNumbers.forEach(num => {
+    const button = document.createElement("button");
+    button.classList.add("number-button");
+    button.id = `${num}`;
+    button.textContent = `${num}`;
+    button.style = `background-color: ${buttonBgColors[Math.ceil(num/3 + 0.1)]}; border: 0.25vw solid ${buttonBorderColors[Math.ceil(num/3 + 0.1)]};`;
+    numbers.appendChild(button);
+});
+
+const operatorSymbols = ['+', '-', 'x', '/', '='];
+const operators = document.querySelector("#operators");
+
+let operatorCount = 0;
+operatorSymbols.forEach(operator => {
+    const button = document.createElement("button");
+    button.classList.add("operator-button");
+    button.id = `${operator}`;
+    button.textContent = `${operator}`;
+    button.style = `background-color: ${buttonBgColors[operatorCount]}; border: 0.25vw solid ${buttonBorderColors[operatorCount]};`;
+    operators.appendChild(button);
+    operatorCount++;
+});
